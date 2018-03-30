8afee95a382b6297fb1d8c1d484afe800739c204
hjain <hj@gmail.com>
2018-03-30 20:19:06 +0530

    first commit

---
+++ WindowsApplication4/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Teal
+        Me.Panel1.Controls.Add(Me.Label16)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label15)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label14)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label13)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label12)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label11)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Location = New System.Drawing.Point(73, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(184, 204)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label16
+        '
+        Me.Label16.BackColor = System.Drawing.Color.White
+        Me.Label16.Location = New System.Drawing.Point(141, 156)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(40, 40)
+        Me.Label16.TabIndex = 16
+        Me.Label16.Text = "Label16"
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(3, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 40)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label15
+        '
+        Me.Label15.BackColor = System.Drawing.Color.White
+        Me.Label15.Location = New System.Drawing.Point(95, 156)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(40, 40)
+        Me.Label15.TabIndex = 15
+        Me.Label15.Text = "Label15"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(49, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 40)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label14
+        '
+        Me.Label14.BackColor = System.Drawing.Color.White
+        Me.Label14.Location = New System.Drawing.Point(49, 156)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(40, 40)
+        Me.Label14.TabIndex = 14
+        Me.Label14.Text = "Label14"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(95, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 40)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Label3"
+        '
+        'Label13
+        '
+        Me.Label13.BackColor = System.Drawing.Color.White
+        Me.Label13.Location = New System.Drawing.Point(3, 156)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(40, 40)
+        Me.Label13.TabIndex = 13
+        Me.Label13.Text = "Label13"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(141, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 40)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Label4"
+        '
+        'Label12
+        '
+        Me.Label12.BackColor = System.Drawing.Color.White
+        Me.Label12.Location = New System.Drawing.Point(141, 107)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(40, 40)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = "Label12"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(3, 58)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 40)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Label5"
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.Color.White
+        Me.Label11.Location = New System.Drawing.Point(95, 107)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(40, 40)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "Label11"
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(49, 58)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 40)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Label6"
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.Color.White
+        Me.Label10.Location = New System.Drawing.Point(49, 107)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(40, 40)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "Label10"
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(95, 58)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 40)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Label7"
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.Color.White
+        Me.Label9.Location = New System.Drawing.Point(3, 107)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(40, 40)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Label9"
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.White
+        Me.Label8.Location = New System.Drawing.Point(141, 58)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 40)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Label8"
+        '
+        'label17
+        '
+        Me.label17.Location = New System.Drawing.Point(355, 37)
+        Me.label17.Name = "label17"
+        Me.label17.Size = New System.Drawing.Size(102, 24)
+        Me.label17.TabIndex = 1
+        Me.label17.Text = "SCORE BOARD"
+        '
+        'Label18
+        '
+        Me.Label18.Location = New System.Drawing.Point(477, 37)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(100, 23)
+        Me.Label18.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(637, 261)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.label17)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents label17 As System.Windows.Forms.Label
+    Friend WithEvents Label18 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication4/Form1.vb
@@ -0,0 +1,446 @@
+﻿Public Class Form1
+    Dim r As Random = New Random
+    Dim a(,) As Integer = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
+    Dim b(,) As Integer = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
+    Dim old(,) As Integer = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
+    Dim score As Integer = 0
+    Dim spot As Integer
+    Dim temp As Integer
+   
+    Private Sub updatearray()
+        If a(0, 0) = 0 Then
+            Label1.Text = ""
+        Else
+            Label1.Text = a(0, 0)
+        End If
+        If a(0, 1) = 0 Then
+            Label2.Text = ""
+        Else
+            Label2.Text = a(0, 1)
+        End If
+        If a(0, 2) = 0 Then
+            Label3.Text = ""
+        Else
+            Label3.Text = a(0, 2)
+        End If
+        If a(0, 3) = 0 Then
+            Label4.Text = ""
+        Else
+            Label4.Text = a(0, 3)
+        End If
+        If a(1, 0) = 0 Then
+            Label5.Text = ""
+        Else
+            Label5.Text = a(1, 0)
+        End If
+        If a(1, 1) = 0 Then
+            Label6.Text = ""
+        Else
+            Label6.Text = a(1, 1)
+        End If
+        If a(1, 2) = 0 Then
+            Label7.Text = ""
+        Else
+            Label7.Text = a(1, 2)
+        End If
+        If a(1, 3) = 0 Then
+            Label8.Text = ""
+        Else
+            Label8.Text = a(1, 3)
+        End If
+        If a(2, 0) = 0 Then
+            Label9.Text = ""
+        Else
+            Label9.Text = a(2, 0)
+        End If
+        If a(2, 1) = 0 Then
+            Label10.Text = ""
+        Else
+            Label10.Text = a(2, 1)
+        End If
+        If a(2, 2) = 0 Then
+            Label11.Text = ""
+        Else
+            Label11.Text = a(2, 2)
+        End If
+        If a(2, 3) = 0 Then
+            Label12.Text = ""
+        Else
+            Label12.Text = a(2, 3)
+        End If
+        If a(3, 0) = 0 Then
+            Label13.Text = ""
+        Else
+            Label13.Text = a(3, 0)
+        End If
+        If a(3, 1) = 0 Then
+            Label14.Text = ""
+        Else
+            Label14.Text = a(3, 1)
+        End If
+        If a(3, 2) = 0 Then
+            Label15.Text = ""
+        Else
+            Label15.Text = a(3, 2)
+        End If
+        If a(3, 3) = 0 Then
+            Label16.Text = ""
+        Else
+            Label16.Text = a(3, 3)
+        End If
+    End Sub
+    Private Sub left1()
+        Dim i As Integer
+        Dim j As Integer
+        Dim k As Integer
+        For i = 0 To 3
+            For j = 0 To 2
+                k = j
+                Do
+                    k = k + 1
+                Loop Until a(i, k) <> 0 Or k = 3
+                If a(i, j) = a(i, k) Then
+                    a(i, k) = 0
+                    a(i, j) = 2 * a(i, j)
+                    score += a(i, j)
+                End If
+            Next
+            For j = 0 To 2
+                If a(i, j) = 0 Then
+                    k = j
+                    Do
+                        k = k + 1
+                    Loop Until a(i, k) <> 0 Or k = 3
+                    If a(i, k) <> 0 Then
+                        a(i, j) = a(i, k)
+                        a(i, k) = 0
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub right1()
+        Dim i As Integer
+        Dim j As Integer
+        Dim k As Integer
+        For i = 0 To 3
+            For j = 3 To 1 Step -1
+                k = j
+                Do
+                    k = k - 1
+                Loop Until a(i, k) <> 0 Or k = 0
+                If a(i, j) = a(i, k) Then
+                    a(i, k) = 0
+                    a(i, j) = 2 * a(i, j)
+                    score += a(i, j)
+                End If
+            Next
+            For j = 3 To 1 Step -1
+                If a(i, j) = 0 Then
+                    k = j
+                    Do
+                        k = k - 1
+                    Loop Until a(i, k) <> 0 Or k = 0
+                    If a(i, k) <> 0 Then
+                        a(i, j) = a(i, k)
+                        a(i, k) = 0
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub up()
+        Dim i2 As Integer
+        Dim j2 As Integer
+        Dim k2 As Integer
+        For i2 = 0 To 3
+            For j2 = 0 To 2
+                k2 = j2
+                Do
+                    k2 = k2 + 1
+                Loop Until a(k2, i2) <> 0 Or k2 = 3
+                If a(j2, i2) = a(k2, i2) Then
+                    a(k2, i2) = 0
+                    a(j2, i2) = 2 * a(j2, i2)
+                    score += a(j2, i2)
+                End If
+            Next
+            For j2 = 0 To 2
+                If a(j2, i2) = 0 Then
+                    k2 = j2
+                    Do
+                        k2 = k2 + 1
+                    Loop Until a(k2, i2) <> 0 Or k2 = 3
+                    If a(k2, i2) <> 0 Then
+                        a(j2, i2) = a(k2, i2)
+                        a(k2, i2) = 0
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub down()
+        Dim i As Integer
+        Dim j As Integer
+        Dim k As Integer
+        For i = 0 To 3
+            For j = 3 To 1 Step -1
+                k = j
+                Do
+                    k = k - 1
+                Loop Until a(k, i) <> 0 Or k = 0
+                If a(j, i) = a(k, i) Then
+                    a(k, i) = 0
+                    a(j, i) = 2 * a(j, i)
+                    score += a(j, i)
+                End If
+            Next
+            For j = 3 To 1 Step -1
+                If a(j, i) = 0 Then
+                    k = j
+                    Do
+                        k = k - 1
+                    Loop Until a(k, i) <> 0 Or k = 0
+                    If a(k, i) <> 0 Then
+                        a(j, i) = a(k, i)
+                        a(k, i) = 0
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim i As Integer = r.Next(0, 4)
+        Dim j As Integer = r.Next(0, 4)
+        Dim k As Integer = r.Next(0, 2)
+        If k = 0 Then
+            a(i, j) = 2
+        ElseIf k = 1 Then
+            a(i, j) = 4
+        End If
+        Dim i1 As Integer = r.Next(0, 4)
+        Dim j1 As Integer = r.Next(0, 4)
+        Dim k1 As Integer = r.Next(0, 2)
+        While i >= 0
+            If i1 = i And j1 = j Then
+                i1 = r.Next(0, 3)
+                j1 = r.Next(0, 3)
+            Else
+                If k1 = 0 Then
+                    a(i1, j1) = 2
+                    Exit While
+                ElseIf k1 = 1 Then
+                    a(i1, j1) = 4
+                    Exit While
+                End If
+            End If
+        End While
+        updatearray()
+    End Sub
+    
+    Private Sub addnum()
+        Dim i1 As Integer = r.Next(0, 4)
+        Dim j1 As Integer = r.Next(0, 4)
+        Dim k1 As Integer = r.Next(0, 2)
+        While i1 >= 0
+            If a(i1, j1) <> 0 Then
+                i1 = r.Next(0, 4)
+                j1 = r.Next(0, 4)
+            Else
+                If k1 = 0 Then
+                    a(i1, j1) = 2
+                    Exit While
+                ElseIf k1 = 1 Then
+                    a(i1, j1) = 4
+                    Exit While
+                End If
+            End If
+        End While
+    End Sub
+    Private Sub updateold()
+        For i = 0 To 3
+            For j = 0 To 3
+                old(i, j) = a(i, j)
+            Next
+        Next
+    End Sub
+    Private Sub checknew()
+
+        temp = 0
+        For i = 0 To 3
+            For j = 0 To 3
+                If a(i, j) = old(i, j) Then
+                    temp += 1
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub checkend()
+        Dim clow As Integer = 0
+        For i = 0 To 3
+            For j = 0 To 3
+                If a(i, j) <> 0 Then
+                    clow += 1
+
+                End If
+            Next
+        Next
+        If clow = 16 Then
+            For i1 = 0 To 3
+                For j1 = 0 To 3
+                    b(i1, j1) = a(i1, j1)
+                Next
+            Next
+            left1()
+            clow = 0
+            For i = 0 To 3
+                For j = 0 To 3
+                    If a(i, j) = b(i, j) Then
+                        clow += 1
+
+                    End If
+                Next
+            Next
+            If clow = 16 Then
+                right1()
+                clow = 0
+                For i = 0 To 3
+                    For j = 0 To 3
+                        If a(i, j) = b(i, j) Then
+                            clow += 1
+
+                        End If
+                    Next
+                Next
+                If clow = 16 Then
+                    up()
+                    clow = 0
+                    For i = 0 To 3
+                        For j = 0 To 3
+                            If a(i, j) = b(i, j) Then
+                                clow += 1
+
+                            End If
+                        Next
+                    Next
+                    If clow = 16 Then
+                        down()
+                        clow = 0
+                        For i = 0 To 3
+                            For j = 0 To 3
+                                If a(i, j) = b(i, j) Then
+                                    clow += 1
+
+                                End If
+                            Next
+                        Next
+                        If clow = 16 Then
+                            MsgBox("GAME OVER")
+
+                        Else
+                            For i1 = 0 To 3
+                                For j1 = 0 To 3
+                                    a(i1, j1) = b(i1, j1)
+                                Next
+                            Next
+                            Exit Sub
+
+                        End If
+
+                    Else
+                        For i1 = 0 To 3
+                            For j1 = 0 To 3
+                                a(i1, j1) = b(i1, j1)
+                            Next
+                        Next
+                        Exit Sub
+
+                    End If
+                Else
+                    For i1 = 0 To 3
+                        For j1 = 0 To 3
+                            a(i1, j1) = b(i1, j1)
+                        Next
+                    Next
+                    Exit Sub
+
+                End If
+            Else
+                For i1 = 0 To 3
+                    For j1 = 0 To 3
+                        a(i1, j1) = b(i1, j1)
+                    Next
+                Next
+                Exit Sub
+            End If
+        Else
+            Exit Sub
+
+        End If
+
+
+    End Sub
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = Keys.Up Then
+            up()
+            checknew()
+            If temp = 16 Then
+
+            Else
+
+                addnum()
+                score += spot
+            End If
+
+            updatearray()
+            updateold()
+            Label18.Text = score
+            checkend()
+
+        End If
+        If e.KeyCode = Keys.Down Then
+            down()
+            checknew()
+            If temp = 16 Then
+
+            Else
+
+                addnum()
+            End If
+            updatearray()
+            updateold()
+            Label18.Text = score
+            checkend()
+        End If
+        If e.KeyCode = Keys.Right Then
+            right1()
+            checknew()
+            If temp = 16 Then
+
+            Else
+
+                addnum()
+            End If
+            updatearray()
+            updateold()
+            Label18.Text = score
+            checkend()
+        End If
+        If e.KeyCode = Keys.Left Then
+            left1()
+            checknew()
+            If temp = 16 Then
+
+            Else
+
+                addnum()
+            End If
+            updatearray()
+            updateold()
+            Label18.Text = score
+            checkend()
+        End If
+    End Sub
+
+End Class
